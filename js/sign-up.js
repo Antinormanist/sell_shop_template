66d80b88cf5f7eea439cd11cb5d98c804feaeb35
Antinormanist <katya.goryacheva.2009@gmail.com>
2024-08-01 19:16:13 +1000

    Sign up is done and js files are fixed(add validation)

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,29 +1,35 @@
 const form = document.querySelector('.menu')
 const label = document.querySelector('.hidden')
-const inp = document.querySelector('#code')
+const inp = document.querySelector('#hidden')
+
 let code
 let tries = 5
 let eol_type
 
-
 form.addEventListener('sumbit', function(event) {
     event.preventDefault();
     const formData = new FormData(form)
+    const email = formData.get('email')
+    const username = formData.get('username')
+    const password1 = formData.get('password1')
+    const password2 = formData.get('password2')
 
-    const eol = formData.get('eol')
-    const password = formData.get('password')
-    // CHECK DATA ON VALIDATION
-    if (eol.contains('@')) {
+    // VALIDATION
+    if (password1 != password2) {
+        window.location = 'URL'
+    }
+
+    if (email.contains('@')) {
         eol_type = 'email'
     } else {
         eol_type = 'login'
     }
 
+    // OTHER BORING THINGS
     if (code) {
         if (tries === 0) {
             tries = 5
             code = undefined
-            // REMOVE CODE INPUT
             label.classList.add('hidden')
             inp.classList.add('hidden')
         } else {
@@ -32,18 +38,16 @@ form.addEventListener('sumbit', function(event) {
                 $.ajax({
                     type: 'POST',
                     url: 'URL',
-                    data: {success: true},
+                    data: {success: true}
                 })
-                window.location = 'URL'
             }
             tries -= 1
         }
     } else {
         $.ajax({
             type: 'POST',
-            // PUT URL HERE
-            url: 'url',
-            data: {eol: [eol, eol_type], password: password},
+            url: 'URL',
+            data: {email: email, username: username, password: password1},
             success: function(response) {
                 code = JSON.parse(response).code
                 if (label.classList.contains('hidden')) {
@@ -56,5 +60,4 @@ form.addEventListener('sumbit', function(event) {
             }
         })
     }
-    
 })
